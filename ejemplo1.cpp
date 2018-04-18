c534beba3e1d277b43496cda8def192394e029bf
Margoth <magolhis@gmail.com>
2018-04-18 14:50:33 -0500

    Primera subida de archivo

---
+++ ejemplo1.cpp
@@ -0,0 +1,13 @@
+Proceso suma
+	Escribir 'Hola';
+	Definir x,z Como Entero;
+	Definir s Como Entero;
+	Escribir 'Ingrese un número';
+	Leer x;
+	Escribir 'Escribir un número';
+	Leer z;
+	s <- x+z;
+	Escribir 'El resultado de la suma es: ';
+	Escribir s;
+FinProceso
+
